626a3301a60b217f74adcf1d47ac7c21cf623cff
mendamgalan <mendamgalanjargal@gmail.com>
2026-06-24 16:49:21 +0800

    deployment fixes

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,4 +2,12 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL);
 
+socket.on("connect", () => {
+  console.log("Socket connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Socket error:", err);
+});
+
 export default socket;
